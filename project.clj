e7fca37763e517702ce85e26eff03918631bc7fe
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-02-08 01:30:13 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.grpc/grpc-netty-shaded "1.26.0"]
                  [io.grpc/grpc-protobuf "1.26.0"]
                  [io.grpc/grpc-stub "1.26.0"]
                  [org.vdaas.vald/vald-client-java "0.0.2"]]
@@ -27,8 +26,6 @@
                       "--no-server"
                       "--report-unsupported-elements-at-runtime"
                       "--initialize-at-build-time"
-                      "--initialize-at-run-time=io.grpc.netty.shaded.io.netty.buffer.PoolChunk"
-                      "--initialize-at-run-time=io.grpc.netty.shaded.io.netty.buffer.UnpooledByteBufAllocator$InstrumentedUnpooledUnsafeDirectByteBuf"
                       "--allow-incomplete-classpath"
                       "--verbose"
                       "-J-Xms1g"
@@ -52,8 +49,6 @@
                       "--no-server"
                       "--report-unsupported-elements-at-runtime"
                       "--initialize-at-build-time"
-                      "--initialize-at-run-time=io.grpc.netty.shaded.io.netty.buffer.PoolChunk"
-                      "--initialize-at-run-time=io.grpc.netty.shaded.io.netty.buffer.UnpooledByteBufAllocator$InstrumentedUnpooledUnsafeDirectByteBuf"
                       "--allow-incomplete-classpath"
                       "--verbose"
                       "--static"

7ff475f772e58eed72d5676c070b3bed3135676a
kevindiu <kevin_diu@yahoo.com.hk>
2022-12-19 12:10:53 +0900

    update dependencies
    Signed-off-by: kevindiu <kevin_diu@yahoo.com.hk>

--- project.clj
+++ project.clj
@@ -8,26 +8,26 @@
                                     :password :env/clojars_pass
                                     :url "https://clojars.org/repo"}]]
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.grpc/grpc-api "1.47.0"]
-                 [io.grpc/grpc-core "1.47.0"
+                 [io.grpc/grpc-api "1.51.1"]
+                 [io.grpc/grpc-core "1.51.1"
                   :exlusions [io.grpc/grpc-api]]
-                 [io.grpc/grpc-protobuf "1.47.0"]
-                 [io.grpc/grpc-stub "1.47.0"]
-                 [io.envoyproxy.protoc-gen-validate/pgv-java-stub "0.6.7"]
+                 [io.grpc/grpc-protobuf "1.51.1"]
+                 [io.grpc/grpc-stub "1.51.1"]
+                 [io.envoyproxy.protoc-gen-validate/pgv-java-stub "0.6.13"]
                  [org.vdaas.vald/vald-client-java
                   #=(subs
                       #=(clojure.string/trim
                           #=(slurp "VALD_CLIENT_CLJ_VERSION")) 1)]]
   :repl-options {:init-ns vald-client-clj.core}
   :profiles {:dev
-             {:dependencies [[io.grpc/grpc-okhttp "1.47.0"
+             {:dependencies [[io.grpc/grpc-okhttp "1.51.1"
                               :exclusions [io.grpc/grpc-core]]
                              [metosin/jsonista "0.3.3"]
                              [camel-snake-kebab "0.4.2"]]}
              :cmd
              {:source-paths ["cmd"]
               :dependencies [[org.clojure/tools.cli "1.0.206"]
-                             [io.grpc/grpc-okhttp "1.47.0"
+                             [io.grpc/grpc-okhttp "1.51.1"
                               :exclusions [io.grpc/grpc-core]]
                              [metosin/jsonista "0.3.3"]
                              [camel-snake-kebab "0.4.2"]]

3c974667876f50557637e3f71065f96455b72ed6
Kosuke Morimoto <ksk@vdaas.org>
2024-10-08 11:10:20 +0900

    fix sync version failure (#206)
    * fix sync version failure
    
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>
    
    * fix
    
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>
    
    ---------
    
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>
    (cherry picked from commit 6050f11ca0bff11260c328b9331210656b6b9ef7)

--- project.clj
+++ project.clj
@@ -7,30 +7,30 @@
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
                                     :url "https://clojars.org/repo"}]]
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.grpc/grpc-api "1.58.0"]
-                 [io.grpc/grpc-core "1.58.0"
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [io.grpc/grpc-api "1.66.0"]
+                 [io.grpc/grpc-core "1.66.0"
                   :exlusions [io.grpc/grpc-api]]
-                 [io.grpc/grpc-protobuf "1.58.0"]
-                 [io.grpc/grpc-stub "1.58.0"]
-                 [build.buf/protovalidate "0.1.8"]
+                 [io.grpc/grpc-protobuf "1.66.0"]
+                 [io.grpc/grpc-stub "1.66.0"]
+                 [build.buf/protovalidate "0.2.1"]
                  [org.vdaas.vald/vald-client-java
                   #=(subs
                       #=(clojure.string/trim
                           #=(slurp "VALD_CLIENT_CLJ_VERSION")) 1)]]
   :repl-options {:init-ns vald-client-clj.core}
   :profiles {:dev
-             {:dependencies [[io.grpc/grpc-okhttp "1.58.0"
+             {:dependencies [[io.grpc/grpc-okhttp "1.66.0"
                               :exclusions [io.grpc/grpc-core]]
                              [metosin/jsonista "0.3.7"]
-                             [camel-snake-kebab "0.4.3"]]}
+                             [camel-snake-kebab/camel-snake-kebab "0.4.3"]]}
              :cmd
              {:source-paths ["cmd"]
               :dependencies [[org.clojure/tools.cli "1.0.219"]
-                             [io.grpc/grpc-okhttp "1.58.0"
+                             [io.grpc/grpc-okhttp "1.66.0"
                               :exclusions [io.grpc/grpc-core]]
                              [metosin/jsonista "0.3.7"]
-                             [camel-snake-kebab "0.4.3"]]
+                             [camel-snake-kebab/camel-snake-kebab "0.4.3"]]
               :aot :all
               :global-vars {*assert* false}
               :main vald-client-clj.cmd}})
